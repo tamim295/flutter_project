6f042b47d183c3b7c438b999a650490616febd24
tamim295 <tamim65k@gmail.com>
2023-12-26 16:46:52 +0600

    DAY 4 complete login IMAGE

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,13 +4,48 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/login_image.png",
+              fit: BoxFit.fitWidth,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 25.0, horizontal: 50.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your username", labelText: "USERNAME"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter your password", labelText: "PASSWORD"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("LOGIN"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi BOT!");
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
